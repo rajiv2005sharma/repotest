d36532b93b3347312b9710edaa175702297fee7f
Rajiv <rajiv2005.sharma@gmail.com>
2021-11-05 14:16:06 +0530

    Checked in by Rajiv

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Learning Git");
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
 }
